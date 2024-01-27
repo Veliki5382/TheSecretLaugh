671c6ad8d6a42ef26a23d0cc373c518b498b3976
FikusM9 <fikusm123@gmail.com>
2024-01-27 13:33:58 +0100

    EnemySudaranje

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -76,7 +76,7 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Wall")
+        if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Enemy")
         {
             horizontal *= -1;
         }
