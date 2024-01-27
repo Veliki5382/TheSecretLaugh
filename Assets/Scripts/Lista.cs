edf3c803c07bafb4e2c14aa9cfe180b913b254bd
FikusM9 <fikusm123@gmail.com>
2024-01-28 00:28:51 +0100

    lista

---
+++ Assets/Scripts/Lista.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lista : MonoBehaviour
+{
+    public Sprite[] smileSprite;
+    public AudioClip[] soundSprite;
+
+    private void Start()
+    {
+        
+    }
+}
