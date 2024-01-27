6d3491348e93a18c0a83c3e16f4ba521efabb089
FikusM9 <fikusm123@gmail.com>
2024-01-27 19:48:44 +0100

    MainManu

---
+++ Assets/Scripts/MainManu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public static int highscore;
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
