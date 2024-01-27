9ee4c97f2d22c91373f71248174765922056ce01
FikusM9 <fikusm123@gmail.com>
2024-01-27 01:02:59 +0100

    Enemy
    Enemy
    i player prolazi lepo kroz platformu

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,38 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
-public class Player : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Transform groundCheckLeft;
     public Transform groundCheckRight;
     public LayerMask groundLayer;
     public float ms = 8f;
-    public float jumpingPower = 16f;
     public Collider2D colider;
+    public float CD = 0f;
 
     private float horizontal;
-    private float groundedCD;
-    private bool canJump;
+    private bool isJumping;
+    private float jumpCDmax;
+    private float jumpCD;
 
     void Start()
     {
-
+        horizontal = 1f;
+        jumpCD= 0f;
+        jumpCDmax = Random.Range(2,6);
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * ms, rb.velocity.y);
-        if (!IsGrounded()) groundedCD += Time.fixedDeltaTime;
-        else groundedCD = 0;
-        if (groundedCD <= 0.1f) canJump = true;
-        else canJump = false;
         if (rb.velocity.y > 0) colider.isTrigger = true;
         else colider.isTrigger = false;
+        jumpCD += Time.fixedDeltaTime;
+        if (jumpCD >= jumpCDmax)
+        {
+            Jump(Random.Range(15,25));
+            jumpCD = 0;
+            jumpCDmax= Random.Range(2, 6);
+        }
     }
 
     private bool IsGrounded()
@@ -40,21 +44,11 @@ public class Player : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheckLeft.position, 0.02f, groundLayer) || Physics2D.OverlapCircle(groundCheckRight.position, 0.02f, groundLayer);
     }
 
-    public void Move(InputAction.CallbackContext context)
+    public void Jump(float jumpingPower)
     {
-        horizontal = context.ReadValue<Vector2>().x;
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if(context.performed && canJump)
+        if (IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
-
-        if(context.canceled && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
     }
 }
