38b81ebca4c9ec02cf59ca3f9fd1479c4bb33866
Veliki5382 <kuzmanov03@gmail.com>
2024-01-28 15:10:44 +0100

    balanced

--- Assets/Scripts/Papir2.cs
+++ Assets/Scripts/Papir2.cs
@@ -35,7 +35,7 @@ public class Papir2 : MonoBehaviour
     {
         //print(rt.transform.position);
         //print(pomeranjeLevo);
-        if (Player.score >= 1) moze = true;
+        if (Player.score >= 2) moze = true;
         if (pomeranjeLevo && rt.transform.position.x >= -120)
         {
             rt.transform.position = rt.transform.position - new Vector3(Time.fixedDeltaTime * ms, 0, 0);
